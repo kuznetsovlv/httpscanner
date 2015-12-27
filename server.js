73d369190e077650e56af10a200ac711fa43e6a2
leonid <kuznetsovlv1981@gmail.com>
2015-12-28 01:58:43 +0300

    server.js: mime types added

--- server.js
+++ server.js
@@ -11,6 +11,20 @@
 		defaultFile: 'index.html'
 	}
 
+	var mime_types = {
+		".txt": {"Content-Type": "text/plain", "Content-Encoding": "charset=utf-8"},
+		".html": {"Content-Type": "text/html", "Content-Encoding": "charset=utf-8"},
+		".js": {"Content-Type": "text/javascript", "Content-Encoding": "charset=utf-8"},
+		".css": {"Content-Type": "text/css", "Content-Encoding": "charset=utf-8"},
+		".bin": {"Content-Type": "application/binary"},
+		".jpeg": {"Content-Type": "image/jpeg"},
+		".jpg": {"Content-Type": "image/jpeg"},
+		".png": {"Content-Type": "image/png"},
+		".mp4": {"Content-Type": "video/mp4"},
+		".mp3": {"Content-Type": "audio/mp3"},
+		".gif": {"Content-Type": "image/gif"}
+	};
+
 	function sendFile (response, fd, pos) {
 		var size = 1024;
 		fs.read(fd, new Buffer(size), 0, size, pos, function (err, bytesRead, buffer) {
@@ -38,8 +52,9 @@
 				send404(response, 'File ' + path.basename(url) + ' not found.');
 			} else if (stats.isFile()) {
 				response.statusCode = 200;
-				response.setHeader("Content-Type", "text/html");
-				response.setHeader("Content-Encoding", "UTF-8");
+				var headers = mime_types[path.extname(url)] || mime_types['.txt'];
+				for (var key in headers)
+					response.setHeader(key, headers[key]);
 				fs.open(url, 'r', function (err, fd) {
 					if (err)
 								throw err;
