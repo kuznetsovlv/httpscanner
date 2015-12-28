a800ff7650cbd863e52f0a94a89b6466f3e86899
leonid <kuznetsovlv1981@gmail.com>
2015-12-29 02:01:23 +0300

    Server part improved

--- server.js
+++ server.js
@@ -7,24 +7,15 @@
 	var path = require('path');
 	var Buffer = require('buffer').Buffer;
 
+	var scanner = require('./lib/scanner.js');
+	var set_status = require('./lib/status.js');
+	var set_mime = require('./lib/mime.js')
+
 	var options = {
 		defaultFile: 'index.html'
 	}
 
-	var mime_types = {
-		".txt": {"Content-Type": "text/plain", "Content-Encoding": "charset=utf-8"},
-		".htm": {"Content-Type": "text/html", "Content-Encoding": "charset=utf-8"},
-		".html": {"Content-Type": "text/html", "Content-Encoding": "charset=utf-8"},
-		".js": {"Content-Type": "text/javascript", "Content-Encoding": "charset=utf-8"},
-		".css": {"Content-Type": "text/css", "Content-Encoding": "charset=utf-8"},
-		".bin": {"Content-Type": "application/binary"},
-		".jpeg": {"Content-Type": "image/jpeg"},
-		".jpg": {"Content-Type": "image/jpeg"},
-		".png": {"Content-Type": "image/png"},
-		".mp4": {"Content-Type": "video/mp4"},
-		".mp3": {"Content-Type": "audio/mp3"},
-		".gif": {"Content-Type": "image/gif"}
-	};
+	
 
 	function sendFile (response, fd, pos) {
 		var size = 1024;
@@ -41,21 +32,19 @@
 		});
 	}
 
-	function send404 (response, msg) {
-		response.statusCode = 404;
-		response.statusMessage = msg || 'Not found';
+	function sendError (response, code, msg) {
+		set_status(response, code || 404, msg);
 		response.end();
 	} 
 
 	function urlToFile (response, url) {
 		fs.stat(url, function (err, stats) {
 			if (err) {
-				send404(response, 'File ' + path.basename(url) + ' not found.');
+				sendError(response, 404, 'File ' + path.basename(url) + ' not found.');
 			} else if (stats.isFile()) {
 				response.statusCode = 200;
-				var headers = mime_types[path.extname(url)] || mime_types['.txt'];
-				for (var key in headers)
-					response.setHeader(key, headers[key]);
+				response.statusMessage = 'OK';
+				set_mime(response, path.extname(url));
 				fs.open(url, 'r', function (err, fd) {
 					if (err)
 								throw err;
@@ -64,7 +53,7 @@
 			} else if (stats.isDirectory()) {
 				urlToFile (response, path.normalize(path.join(url, options.defaultFile)));
 			} else {
-				send404(response, 'Incorrect path');
+				sendError(response, 404, 'Incorrect path');
 			}
 		});
 	}
@@ -74,7 +63,34 @@
 	server.on('request', function (request, response) {
 		switch (request.method) {
 			case 'GET': urlToFile(response, path.join(__dirname, request.url)); break;
-			case 'POST': break;
+			case 'POST':
+				var data = [];
+				request.on('data', function (chunk) {data.push(chunk);});
+				request.on('end', function () {
+					data = data.join('');
+					if (!request.headers['content-type'] === 'application/json')
+						sendError(response, 400, 'Content must be application/json');
+					data = JSON.parse(data);
+					var params = [],
+					    type = ['.', data['--format'] || 'png'].join('');
+					for (var key in data) {
+						params.push(data[key] ? [key, data[key]].join(' ') : key);
+
+					}
+					var result = scanner(params);
+					response.statusCode = null;
+					result.stdout.on('data', function (data) {
+						if (!response.statusCode) {
+							response.statusCode = 200;
+							response.statusMessage = 'OK';
+							set_mime(response, type);
+						}
+						response.write(data);
+					});
+					result.stdout.on('end', function () {response.end();});
+					result.stderr.on('data', function (data) {sendError(response, 500);});
+				});
+				break;
 		}
 	});
 
