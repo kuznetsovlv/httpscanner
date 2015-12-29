8d4f2d856e3ba8407d651849aa55a6f790694de9
leonid <kuznetsovlv1981@gmail.com>
2015-12-29 16:08:09 +0300

    js/scanner.js: Scanner.ask() method complited

--- js/scanner.js
+++ js/scanner.js
@@ -1,39 +1,5 @@
 (function () {
 	"use strict";
-	 function sendResponse (resp, callback) {
-	 	var request = new XMLHttpRequest();
-	 	request.open("POST", "");
-
-	 	request.onreadystatechange = function () {
-	 		if (request.readyState == 4 && request.status == 200)
-	 			callback(request);
-	 	};
-
-	 	request.ontimeout = function () {
-	 		alert('Timeout!');
-	 	};
-
-	 	/*request.onprogress = function () {
-	 		console.log(request.responseText.length);
-	 	}*/
-
-	 	if (typeof resp === 'object') {
-	 		resp = JSON.stringify(resp);
-	 		request.setRequestHeader("Content-Type", "application/json");
-	 	} else {
-	 		request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
-	 	}
-	 	request.send(resp);
-	 }
-
-	 function devParse (str) {
-	 	var device = {};
-	 	str =( str.split('device ')[1] || '').split(' is a ');
-	 	device.name = str[0].replace(/[`'"]/g, ' ').trim();
-	 	device.description = str[1] || '';
-	 	return device;
-	 }
-
 	 function Scanner () {
 
 	 }
@@ -43,34 +9,19 @@
 	 		callback = params;
 	 		params = {};
 	 	}
-	 	var _callback = callback,
-	 	    self = this;;
-	 	switch (cmd) {
-	 		case 'list':
-	 			params = {'-L': ''};
-	 			callback = function (obj) {
-	 				var tmp = obj.responseText.split('\n'),
-	 				    result = [];
+	 	var self = this;
+	 	params.cmd = cmd;
 
-	 				for (var i = 0, l = tmp.length; i < l; ++i){
-	 					var t = tmp[i];
-	 					if (t)
-	 						result.push(devParse(t));
-	 				}
-
-	 				_callback.call(self, result);
-	 			}
-	 			break;
-	 		case 'scan':
-	 			callback = function (obj) {
-	 				_callback.call(self, obj.responseText);
-	 			};
-	 			if (!params)
-	 				params = {};
-
-	 	}
-
-	 	sendResponse.call(this, params, callback);
+	 	var request = new XMLHttpRequest();
+	 	request.open("POST", "");
+	 	request.setRequestHeader("Content-Type", "application/json");
+	 	request.onreadystatechange = function () {
+	 		if (request.readyState == 4 && request.status == 200){
+	 			var result = JSON.parse(request.responseText);
+	 			callback.call(self, result);
+	 		}
+	 	};
+	 	request.send(JSON.stringify(params));
 	 }
 
 	 window.Scanner = Scanner;
